2d0c82988e593250a4601550e3bd36f0cd402c04
Chertopolokhov Alexey <leshii26@yandex.ru>
2023-02-09 00:59:48 +0300

    Initial commit

---
+++ src/main/java/BooleanSearchEngine.java
@@ -0,0 +1,81 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class BooleanSearchEngine implements SearchEngine {
+    protected final String PDFsFolder = "./pdfs";
+    protected final String STOP_WORDS = "stop-ru.txt";
+    //???
+
+    protected Map<String, Integer> wordsList = new HashMap<>();
+
+    public BooleanSearchEngine(File pdfsDir) throws IOException {
+
+        //String[] stopWords =
+
+        for (File pdfFile : Objects.requireNonNull(new File(PDFsFolder).listFiles())) {
+
+            String fileName = pdfFile.getName();
+            System.out.println(fileName);
+
+
+            PdfDocument doc = new PdfDocument(new PdfReader(pdfFile.getPath()));
+            int pages = doc.getNumberOfPages();
+
+            for (int i = 1; i == pages; i++) {
+                System.out.println("jeronimo");
+//
+//                String text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+//                String[] words = (text.toLowerCase()).split("\\P{IsAlphabetic}+"); // получаю массив текстовый
+//             45   int count = 1;
+//
+//                for (String word : words) {
+//                    // if стоп слово cont
+//
+//                    if (!wordsList.containsKey(word)) {
+//                        wordsList.get(word);
+//                        wordsList.put(word, count);
+//                    }
+//                    if (wordsList.containsKey(word)) {
+//                        wordsList.put(word, count++);
+//                    }
+//
+//                }
+//                System.out.println(wordsList.toString());
+            }
+            doc.close();
+        }
+
+        //String fileName = pdfsDir.getName();
+//Чтобы создать объект пдф-документа, нужно указать объект File этого документа следующим классам:
+        //var doc = new PdfDocument(new PdfReader(pdfsDir));
+//Чтобы получить объект одной страницы документа, нужно вызвать:
+        //doc.getPage(1);
+        //doc.getNumberOfPages();//количество страниц в документе
+//Чтобы получить текст со страницы, используйте
+        //var text = PdfTextExtractor.getTextFromPage(doc.getPage(1));
+//Чтобы разбить текст на слова (а они в этих документах разделены могут быть не только пробелами), используйте
+        //var words = text.split("\\P{IsAlphabetic}+"); // получаю массив текстовый
+
+        //PdfReader reader = new PdfReader(pdfsDir.getPath());
+
+
+        // прочтите тут все pdf и сохраните нужные данные,
+        // тк во время поиска сервер не должен уже читать файлы
+    }
+
+    // TODO: 31.01.2023
+    public static String search(String word) {
+        return null;
+    }
+
+//    @Override
+//    public List<PageEntry> search(String word) {
+//        // тут реализуйте поиск по слову
+//        return Collections.emptyList();
+//    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+
+public class Main {
+
+    public final static int PORT = 8989;
+    public static void main(String[] args) throws Exception {
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+//        System.out.println(engine.search("бизнес"));
+//
+//        try (ServerSocket serverSocket = new ServerSocket(PORT);) { // стартуем сервер один(!) раз
+//            while (true) { // в цикле(!) принимаем подключения
+//                try (
+//                        Socket socket = serverSocket.accept();
+//                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+//                ) {
+//                    // обработка одного подключения
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Не могу стартовать сервер");
+//            e.printStackTrace();
+//        }
+
+        // здесь создайте сервер, который отвечал бы на нужные запросы
+        // слушать он должен порт 8989
+        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    }
+}
